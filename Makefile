b1284c356384ddb1c49b07e7c6f8ed3b696422db
Mike Schaeffer <mike@mschaef.com>
2023-08-16 18:14:31 -0400

    Add code to create a window.

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 wator:	wator.c
-	cc wator.c -o wator
+	cc wator.c -o wator -I/opt/homebrew/include/SDL2 -D_THREAD_SAFE -L/opt/homebrew/lib -lSDL2
 
 .PHONY: clean
 clean:
--- wator.c
+++ wator.c
@@ -1,7 +1,65 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+#include <SDL.h>
+
+#define SCREEN_WIDTH    800
+#define SCREEN_HEIGHT   600
 
 int main(int argc, char *argv[]) {
 
-     printf("end run.\n");
-     return 0;
+     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+          printf("SDL could not be initialized!\n"
+                 "SDL_Error: %s\n", SDL_GetError());
+          return 0;
+     }
+
+     SDL_Window *window = SDL_CreateWindow("Wator",
+                                           SDL_WINDOWPOS_UNDEFINED,
+                                           SDL_WINDOWPOS_UNDEFINED,
+                                           SCREEN_WIDTH, SCREEN_HEIGHT,
+                                           SDL_WINDOW_SHOWN);
+     if (!window) {
+          printf("Window could not be created!\n"
+                 "SDL_Error: %s\n", SDL_GetError());
+     } else {
+          SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+          if(!renderer) {
+               printf("Renderer could not be created!\n"
+                      "SDL_Error: %s\n", SDL_GetError());
+          } else {
+            bool quit = false;
+
+            while(!quit)
+            {
+                SDL_Event e;
+
+                SDL_WaitEvent(&e);
+
+                if(e.type == SDL_QUIT) {
+                    quit = true;
+                }
+
+                SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+
+                SDL_RenderClear(renderer);
+
+
+                // rendering goes here
+
+                SDL_RenderPresent(renderer);
+            }
+
+            SDL_DestroyRenderer(renderer);
+        }
+
+        SDL_DestroyWindow(window);
+    }
+
+    SDL_Quit();
+
+
+    printf("end run.\n");
+    return 0;
 }
