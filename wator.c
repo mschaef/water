564c7a134ac39ecfebb83d8a100d10800968eb73
Mike Schaeffer <mike@mschaef.com>
2023-08-19 21:59:34 -0400

    Add a board representation and a mechanism for rendering it to the window.

--- wator.c
+++ wator.c
@@ -6,6 +6,11 @@
 #define SCREEN_WIDTH    800
 #define SCREEN_HEIGHT   600
 
+#define BOARD_WIDTH 10
+#define BOARD_HEIGHT 10
+
+int board[BOARD_WIDTH * BOARD_HEIGHT];
+
 Uint32 update(Uint32 interval, void* param) {
      SDL_Log("update");
 
@@ -16,13 +21,33 @@ void render(SDL_Renderer *renderer) {
      SDL_Log("render\n");
 
      SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
-
      SDL_RenderClear(renderer);
 
-     SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0xFF, 0xFF);
+     for(int xx = 0; xx < BOARD_WIDTH; xx++) {
+          for(int yy = 0; yy < BOARD_HEIGHT; yy++) {
+               int cel = board[xx + yy * BOARD_WIDTH];
 
-     SDL_FRect box = {20, 20, 80, 80};
-     SDL_RenderFillRectF(renderer, &box);
+               SDL_Log("%d, %d = %d", xx, yy, cel);
+
+               if (cel == 0) {
+                    SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF);
+               } if (cel == 1) {
+                    SDL_SetRenderDrawColor(renderer, 0x00, 0xFF, 0x00, 0xFF);
+               } if (cel == 2) {
+                    SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0xFF, 0xFF);
+               }
+
+               SDL_FRect box;
+
+               box.x = xx * 20;
+               box.y = yy * 20;
+               box.w = 20;
+               box.h = 20;
+
+               SDL_RenderFillRectF(renderer, &box);
+          }
+     }
+
 
      SDL_RenderPresent(renderer);
 }
@@ -45,6 +70,10 @@ void main_loop(SDL_Renderer *renderer) {
 }
 
 int main(int argc, char *argv[]) {
+     for(int ii = 0; ii < BOARD_WIDTH * BOARD_HEIGHT; ii++) {
+          board[ii] = rand() % 3;
+     }
+
      if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) < 0) {
           SDL_LogError(SDL_LOG_CATEGORY_ERROR, "SDL could not be initialized! SDL_Error: %s\n", SDL_GetError());
           return 0;
