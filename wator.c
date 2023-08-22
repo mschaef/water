1c2b28b6859fd941b73244963ba440b282c4b50c
Mike Schaeffer <mike@mschaef.com>
2023-08-21 21:10:47 -0400

    Remove erroneous bias in direction update logic.

--- wator.c
+++ wator.c
@@ -81,7 +81,7 @@ static void rand_direction(int *dx, int *dy) {
      case 0: *dx = 0; *dy = -1; break;
      case 1: *dx = 1; *dy = 0; break;
      case 2: *dx = 0; *dy = 1; break;
-     case 3: *dx = -1; *dy = 1; break;
+     case 3: *dx = -1; *dy = 0; break;
      }
 }
 
